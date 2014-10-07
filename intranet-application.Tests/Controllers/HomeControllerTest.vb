748d576e1aae844f9506191c1f8a646e5b2cbb89
Viv Richards <vivrichards600@hotmail.com>
2014-10-07 08:31:48 +0100

    controller test added to ensure our view loads with the correct title

---
+++ intranet-application.Tests/Controllers/HomeControllerTest.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Web.Mvc
+
+Imports Microsoft.VisualStudio.TestTools.UnitTesting.Web
+
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Imports intranet_application.intranet_application
+
+
+
+'''<summary>
+'''This is a test class for HomeControllerTest and is intended
+'''to contain all HomeControllerTest Unit Tests
+'''</summary>
+<TestClass()> _
+Public Class HomeControllerTest
+
+
+    <TestMethod()> Public Sub Index()
+        ' Arrange
+        Dim controller As New HomeController()
+
+        ' Act
+        Dim result As ViewResult = DirectCast(controller.Index(), ViewResult)
+
+        ' Assert
+        Dim viewData As ViewDataDictionary = result.ViewData
+        Assert.AreEqual("Home Page Title", viewData("Title"))
+
+    End Sub
+
+  
+End Class
---
+++ intranet-application.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("intranet-application.Tests")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("intranet-application.Tests")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: CLSCompliant(True)> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("15af583b-07b1-44de-90de-f4f5eaa966d7")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
